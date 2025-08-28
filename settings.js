b1259ee5939aaf31dcf36f8d242cb16a911a9edb
Dimah226 <140923987+Dimah226@users.noreply.github.com>
2025-08-28 22:36:09 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Dimah', // Your name
-  ownerNumber: '2250799142145', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'BAZAAR', // Your name
+  ownerNumber: '2250555938112', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
